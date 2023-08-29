35d7082701c59b16bf433b20a84fb3803c61d38f
Andy Cockroft <acockroft1994@gmail.com>
2023-08-29 15:13:21 +0200

    part 2: error handling and middleware

---
+++ config/allowedOrigins.js
@@ -0,0 +1,5 @@
+const allowedOrigins = [
+    'http://localhost:5005',
+];
+
+module.exports = allowedOrigins;
--- server.js
+++ server.js
@@ -1,22 +1,36 @@
-const app = require("./app");
 const express = require("express");
+const app = express();
 const path = require("path");
+const { logger } = require("./middleware/logger");
+const errorHandler = require("./middleware/errorHandler");
+const cors = require("cors");
+const corsOptions = require("./config/corsOptions");
+const cookieParser = require("cookie-parser");
+
 const PORT = process.env.PORT || 5005;
 
-app.use("/", express.static(path.join(__dirname, "/public")));
+app.use(logger);
+
+app.use(cors(corsOptions));
+
+app.use(express.json());
+
+app.use(cookieParser());
 
 app.use("/", require("./routes/root"));
 
 app.all("*", (req, res) => {
   res.status(404);
   if (req.accepts("html")) {
-    res.sendFile(path.join(__dirname, "views", "404.html"))
+    res.sendFile(path.join(__dirname, "views", "404.html"));
   } else if (req.accepts("json")) {
-    res.json({ message: "404 Not Found"})
+    res.json({ message: "404 Not Found" });
   } else {
-    res.type("txt").send("404 Not Found")
+    res.type("txt").send("404 Not Found");
   }
-})
+});
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server listening on http://localhost:${PORT}`);
