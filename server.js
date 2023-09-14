52281cf138d34ac28666564ad012424929cfbd2b
Andy Cockroft <acockroft1994@gmail.com>
2023-09-14 15:14:57 +0200

    Login Auth in backend complete

--- server.js
+++ server.js
@@ -4,13 +4,14 @@ const app = express();
 const path = require("path");
 const { logger, logEvents } = require("./middleware/logger");
 const errorHandler = require("./middleware/errorHandler");
+const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const corsOptions = require("./config/corsOptions");
-const cookieParser = require("cookie-parser");
-const connectDB = require("./config/dbCon");
+const connectDB = require("./config/dbConn");
 const mongoose = require("mongoose");
+const PORT = process.env.PORT || 3500;
 
-const PORT = process.env.PORT || 5005;
+console.log("Node env:", process.env.NODE_ENV);
 
 connectDB();
 
@@ -18,14 +19,15 @@ app.use(logger);
 
 app.use(cors(corsOptions));
 
-app.use(express.json({ extended: false }));
+app.use(express.json());
 
 app.use(cookieParser());
 
+app.use("/", express.static(path.join(__dirname, "public")));
+
 app.use("/", require("./routes/root"));
-
+app.use("/auth", require("./routes/authRoutes"));
 app.use("/users", require("./routes/userRoutes"));
-
 app.use("/notes", require("./routes/noteRoutes"));
 
 app.all("*", (req, res) => {
@@ -43,9 +45,7 @@ app.use(errorHandler);
 
 mongoose.connection.once("open", () => {
   console.log("Connected to MongoDB");
-  app.listen(PORT, () => {
-    console.log(`Server listening on http://localhost:${PORT}`);
-  });
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 });
 
 mongoose.connection.on("error", (err) => {
